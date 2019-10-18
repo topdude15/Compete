019333b3925f10a00670a17640cb2c5b8cde5943
Trevor Rose <topdude15@gmail.com>
2019-10-17 20:37:45 -0400

    Started working on transmitting object color

--- Assets/Scripts/Utility/ColorSync.cs
+++ Assets/Scripts/Utility/ColorSync.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Normal.Realtime;
 
-public class ColorSync : MonoBehaviour
+public class ColorSync : RealtimeComponent
 {
+    private MeshRenderer _meshRenderer;
+    private ColorSyncModel _model;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _meshRenderer = GetComponent<MeshRenderer>();   
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    private ColorSyncModel model {
+        set {
+            if (_model != null) {
+                _model.colorDidChange -= ColorDidChange;
+            }
+            _model = value;
+
+            if (_model != null) {
+                UpdateMeshRendererColor();
+
+                _model.colorDidChange += ColorDidChange;
+            }
+        }
+    }
+    private void ColorDidChange(ColorSyncModel model, Color value) {
+        UpdateMeshRendererColor();
+    }
+
+    private void UpdateMeshRendererColor() {
+        _meshRenderer.material.color = _model.color;
+    }
+    public void SetColor(Color color) {
+        _model.color = color;
     }
 }
